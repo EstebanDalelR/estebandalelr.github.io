d9171737a78b24169cc60f760e78b375010da1c9
estebandalelr <estebandalelr@gmail.com>
2019-03-14 12:48:54 -0500

    added clinic project

---
+++ clinic/precache-manifest.e614d8faaed896e16decd3bd20431b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "142bcadcfd9c0dd277b9",
+    "url": "/static/js/main.142bcadc.chunk.js"
+  },
+  {
+    "revision": "606e9d457ca74853ab66",
+    "url": "/static/js/2.606e9d45.chunk.js"
+  },
+  {
+    "revision": "142bcadcfd9c0dd277b9",
+    "url": "/static/css/main.0cb569f0.chunk.css"
+  },
+  {
+    "revision": "606e9d457ca74853ab66",
+    "url": "/static/css/2.01614fef.chunk.css"
+  },
+  {
+    "revision": "7efd1fe97fddea4099201b143c7df55e",
+    "url": "/index.html"
+  }
+];
