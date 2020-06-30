79ec6b2d37c7b0669e1e1c8500f154e6c8655a19
estebandalelr <estebandalelr@gmail.com>
2020-06-29 21:43:07 -0500

    Added good looking inline note

--- with-storybook-app/stories/inlineNote.stories.js
+++ with-storybook-app/stories/inlineNote.stories.js
@@ -1,8 +1,9 @@
 import React from "react"
+
 export default {title: "Inline note"}
 
 export const lightMode = () =>(
-    <aside className="bg-gray-200 border-purple-300 border-l-4">
-        This is a note
+    <aside className="py-2 px-3 bg-gray-100 border-purple-300 border-l-4">
+        If you find an error, <a href="https://github.com/EstebanDalelR/estebandalelr.github.io" rel="noopener noreferrer">send me a PR</a> or <a href="https://twitter.com/estebandalelr" rel="noopener noreferrer">write to me on twitter</a>.
     </aside>
 )
