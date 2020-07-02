74bdd562a2c4ccc1ff8928135c38b159edfda4e6
estebandalelr <estebandalelr@gmail.com>
2020-07-02 11:20:45 -0500

    Make more accesible the links

--- with-storybook-app/stories/inlineNote.stories.js
+++ with-storybook-app/stories/inlineNote.stories.js
@@ -4,6 +4,6 @@ export default {title: "Inline note"}
 
 export const lightMode = () =>(
     <aside className="py-2 px-3 bg-gray-100 border-purple-300 border-l-4">
-        If you find an error, <a href="https://github.com/EstebanDalelR/estebandalelr.github.io" rel="noopener noreferrer">send me a PR</a> or <a href="https://twitter.com/estebandalelr" rel="noopener noreferrer">write to me on twitter</a>.
+        Si encuentras un error, <a className="externalLink" href="https://github.com/EstebanDalelR/estebandalelr.github.io" rel="noopener noreferrer" title="github">mándame un PR</a> o <a className="externalLink" href="https://twitter.com/estebandalelr" rel="noopener noreferrer" title="twitter">escríbeme en twitter</a>.
     </aside>
 )
