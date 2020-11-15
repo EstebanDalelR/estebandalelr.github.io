437bb735190057fb8e7408098935e56760b4c624
estebandalelr <estebandalelr@gmail.com>
2020-11-15 17:39:18 -0600

    Added title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
+import Head from 'next/head'
+
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Esteban Dalel R</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
