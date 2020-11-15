9a2f0db0ec563314882e3ada0bf4bc6a834c4de0
estebandalelr <estebandalelr@gmail.com>
2020-11-15 17:54:10 -0600

    added description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Esteban Dalel R</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <title>Esteban Dalel R</title>
+        <meta name="description" content="I make good software and tell bad jokes" />
       </Head>
       <Component {...pageProps} />
     </>
