97b5a15277a6e261807e42e412dff7b1a716d6d5
estebandalelr <estebandalelr@gmail.com>
2020-11-15 17:01:07 -0600

    Finished Card content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 
 export default function IndexPage() {
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="shadow-md">
-        <h1 className="text-2xl">Esteban Dalel R</h1>
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="shadow-md flex justify-center items-center flex-col p-4 rounded">
+        <h1 className="text-3xl">Esteban Dalel R</h1>
         <h2 className="italic">I make good software and tell bad jokes</h2>
+        <p>Find me on:</p>
+        <div className="w-full flex flex-wrap justify-around">
+          <a href="https://twitter.com/EstebanDalelR">Twitter</a>
+          <a href="https://github.com/estebandalelr/">Github</a>
+          <a href="https://www.linkedin.com/in/estebandalelr/">LinkedIn</a>
+        </div>
       </div>
     </div>
   )
