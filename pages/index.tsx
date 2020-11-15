7ab818175bc9bdbec51c57af97830b978708738e
estebandalelr <estebandalelr@gmail.com>
2020-11-15 16:54:04 -0600

    Added more content to card

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ export default function IndexPage() {
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="shadow-md">
-        <h1>Esteban Dalel R</h1>
+        <h1 className="text-2xl">Esteban Dalel R</h1>
+        <h2 className="italic">I make good software and tell bad jokes</h2>
       </div>
     </div>
   )
