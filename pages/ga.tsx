e564a6abeabdc592637dc9080e5fa3da66d03389
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-14 21:04:32 -0500

    Added typography classname

--- pages/ga.tsx
+++ pages/ga.tsx
@@ -1,7 +1,7 @@
 import ga from "../data/ga.json";
 export default function GithubAchievements() {
   return (
-    <div>
+    <div className="prose lg:prose-xl">
       <h1>Github Achievements</h1>
       <p>
         A compilation of <a href='https://github.blog/2022-06-09-introducing-achievements-recognizing-the-many-stages-of-a-developers-coding-journey/'>GitHub Achievements</a>
