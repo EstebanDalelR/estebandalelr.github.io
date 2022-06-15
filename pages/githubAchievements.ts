44aa2758d6a753ac0ffc9fed0283b8d2a5fb0048
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-14 20:32:54 -0500

    Added achievements page

---
+++ pages/githubAchievements.ts
@@ -0,0 +1,11 @@
+export default function GithubAchievements() {
+  return (
+    <div>
+      <h1>Github Achievements</h1>
+      <p>
+        This page is a work in progress. It will be updated as I learn more
+        about the Github API.
+      </p>
+    </div>
+  );
+}
