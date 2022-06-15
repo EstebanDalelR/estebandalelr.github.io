d51f24e0a79bd01492749b2a90c89458905f0473
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-15 10:19:23 -0500

    Direct people to ga data on footer

--- pages/ga.tsx
+++ pages/ga.tsx
@@ -19,7 +19,7 @@ export default function GithubAchievements() {
         })}
       </div>
     </div>
-    <footer>Made by Esteban Dalel R.  <a href="https://github.com/estebandalelr/estebandalelr.github.io">Contribute on GitHub</a></footer>
+    <footer>Made by Esteban Dalel R.  <a href="https://github.com/EstebanDalelR/estebandalelr.github.io/blob/master/data/ga.json">Contribute on GitHub</a></footer>
     </main>
   );
 }
