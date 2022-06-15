033ab09a91bd8b62390e762afe2fbb39e343bd2e
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-14 20:47:57 -0500

    Mapped through achievements

--- pages/ga.tsx
+++ pages/ga.tsx
@@ -1,11 +1,20 @@
+import ga from "../data/ga.json";
 export default function GithubAchievements() {
   return (
     <div>
       <h1>Github Achievements</h1>
       <p>
-        This page is a work in progress. It will be updated as I learn more
-        about the Github API.
+        A compilation of <a href='https://github.blog/2022-06-09-introducing-achievements-recognizing-the-many-stages-of-a-developers-coding-journey/'>GitHub Achievements</a>
       </p>
+      {ga.map((achievement) => {
+        (
+          <div>
+            <h2>{achievement.name}</h2>
+            <p>{achievement.description}</p>
+            <img src={achievement.image} alt={achievement.name} />
+          </div>
+        )
+      })}
     </div>
   );
 }
