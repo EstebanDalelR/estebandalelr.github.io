2ad3d20de711a797b0b102c2b5b7bbbca19c67f4
Esteban Dalel R <estebandalelr@gmail.com>
2022-06-15 09:35:36 -0500

    Made cards for the achievements

--- pages/ga.tsx
+++ pages/ga.tsx
@@ -1,20 +1,22 @@
 import ga from "../data/ga.json";
 export default function GithubAchievements() {
   return (
-    <div className="prose lg:prose-xl">
+    <div className="prose lg:prose-xl px-4">
       <h1>Github Achievements</h1>
       <p>
         A compilation of <a href='https://github.blog/2022-06-09-introducing-achievements-recognizing-the-many-stages-of-a-developers-coding-journey/'>GitHub Achievements</a>
       </p>
-      {ga.map((achievement) => {
-        return (
-          <div>
-            <h2>{achievement.name}</h2>
-            <p>{achievement.description}</p>
-            <img src={achievement.image} alt={achievement.name} />
-          </div>
-        )
-      })}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {ga.map((achievement) => {
+          return (
+            <div className="border-1 border-slate-100 border-solid rounded">
+              <h2>{achievement.name}</h2>
+              <p>{achievement.description}</p>
+              <img src={achievement.image} alt={achievement.name} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
