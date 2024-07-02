23e1e1384871994c5e036924108cd2e64fb9f467
Esteban Dalel R <esteban@astronuts.io>
2024-07-02 12:40:29 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,14 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'accent-1': '#333',
+        "accent-1": "#333",
       },
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
